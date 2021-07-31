4590058b7569be05430bb0965f6260ee408d3ad5
Muhammad Kurnia Sani <80471705+MuhammadKurniaSani-me@users.noreply.github.com>
2021-07-31 08:50:07 +0700

    Update application.js
    1. add an effect setting for the navbar menu
    2. the user will know the current position on the navbar menu

--- javascript_files/aplication.js
+++ javascript_files/aplication.js
@@ -1,2 +1,19 @@
-// pengaturan tombol tema
+// theme button setting
 document.querySelector(".theme-button").addEventListener("click", ()=> {document.body.classList.toggle("dark")});
+
+// nav links underline setting function
+function navbarUnderlineMenu() {
+    const section = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".nav-links");
+    section.forEach(section => {
+        section.addEventListener("mouseenter", function() {
+            const id = this.getAttribute("id");
+            const navActive = document.querySelector(`a[href="#${id}"]`);
+            navLinks.forEach(link => link.classList.remove("active"));
+            console.log(navActive);
+            navActive.classList.add("active");
+        });
+    });
+}
+// run the function
+navbarUnderlineMenu();
